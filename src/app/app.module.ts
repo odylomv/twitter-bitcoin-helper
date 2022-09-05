14c9267db09ce8380acf3fae3c91364aa6de1091
odylomv <odylomv@gmail.com>
2022-09-05 15:49:26 +0300

    add angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
 
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-import { HomeComponent } from './components/home/home.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
 import { TransactionMakerComponent } from './components/transaction-maker/transaction-maker.component';
 
 @NgModule({
     declarations: [AppComponent, HomeComponent, TransactionMakerComponent],
-    imports: [BrowserModule, HttpClientModule, AppRoutingModule, ReactiveFormsModule],
+    imports: [BrowserModule, HttpClientModule, AppRoutingModule, ReactiveFormsModule, BrowserAnimationsModule],
     providers: [],
     bootstrap: [AppComponent],
 })
