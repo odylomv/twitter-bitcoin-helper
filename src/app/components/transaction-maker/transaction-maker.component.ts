ca3cb4101e0a82a5f564d1cf7586c70a000aff48
odylomv <odylomv@gmail.com>
2022-09-06 18:15:39 +0300

    initial transaction maker form

--- src/app/components/transaction-maker/transaction-maker.component.ts
+++ src/app/components/transaction-maker/transaction-maker.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
     selector: 'tbh-transaction-maker',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
     styleUrls: ['./transaction-maker.component.scss'],
 })
 export class TransactionMakerComponent {
+    transactionForm = new FormGroup({
+        utxo: new FormControl<string>('', Validators.required),
+        amount: new FormControl<number>(0, Validators.required),
+        address: new FormControl<string>('', Validators.required),
+    });
+
     constructor() {}
 }
