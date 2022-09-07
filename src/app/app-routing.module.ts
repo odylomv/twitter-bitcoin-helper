f2197b265ad1fb0c83f5234756bd9f3372a17a81
odylomv <odylomv@gmail.com>
2022-09-07 14:30:00 +0300

    modified routing to home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TwitterGuard } from './guards/twitter.guard';
 
 const routes: Routes = [
     {
         path: '',
         loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule),
+        canActivate: [TwitterGuard]
     },
     {
         path: 'transaction-maker',
--- src/app/features/home/home-routing.module.ts
+++ src/app/features/home/home-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { TwitterGuard } from '../../guards/twitter.guard';
 import { HomeComponent } from './home.component';
 
-const routes: Routes = [{ path: '', component: HomeComponent, canActivate: [TwitterGuard] }];
+const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+];
 
 @NgModule({
     imports: [RouterModule.forChild(routes)],
