89df06841a163cd595a5d819dafaa5278d46a6d8
Odysseas Lomvardeas <odylomv@gmail.com>
2022-12-12 01:56:42 +0200

    add functionality back to the modal forms

--- src/app/components/create-modal/create-modal.component.ts
+++ src/app/components/create-modal/create-modal.component.ts
@@ -1,14 +1,50 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { TwitterService } from 'src/app/services/twitter.service';
 
 @Component({
   selector: 'tbh-create-modal',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './create-modal.component.html',
   styleUrls: ['./create-modal.component.scss'],
 })
 export class CreateModalComponent {
   @Input()
   onClose = () => {};
+
+  createForm = new FormGroup({
+    tweetSecret: new FormControl('', Validators.required),
+    imageMethod: new FormControl<'cat' | 'local'>('cat', Validators.required),
+    tweetImage: new FormControl(null),
+    tweetImageSource: new FormControl<File | null>(null),
+    blockchain: new FormControl<'main' | 'test'>('test', Validators.required),
+  });
+
+  constructor(private twitterService: TwitterService) {}
+
+  async createFormSubmit() {
+    console.log(this.createForm);
+    if (
+      this.createForm.controls.tweetSecret.value &&
+      (this.createForm.controls.imageMethod.value || this.createForm.controls.tweetImageSource.value)
+    ) {
+      await this.twitterService.postTweet(
+        this.createForm.controls.tweetSecret.value,
+        this.createForm.controls.imageMethod.value ?? 'cat',
+        this.createForm.controls.tweetImageSource.value,
+        this.createForm.controls.blockchain.value ?? 'test'
+      );
+    }
+  }
+
+  onFileChange(event: any) {
+    if (event.target.files.length > 0) {
+      const file = event.target.files[0];
+      this.createForm.patchValue({
+        tweetImageSource: file,
+      });
+    }
+  }
 }
--- src/app/components/read-modal/read-modal.component.ts
+++ src/app/components/read-modal/read-modal.component.ts
@@ -1,14 +1,29 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { TwitterService } from 'src/app/services/twitter.service';
 
 @Component({
   selector: 'tbh-read-modal',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './read-modal.component.html',
   styleUrls: ['./read-modal.component.scss'],
 })
 export class ReadModalComponent {
   @Input()
   onClose = () => {};
+
+  readForm = new FormGroup({
+    tweetId: new FormControl<string>('', Validators.required),
+  });
+
+  constructor(private twitterService: TwitterService) {}
+
+  async readFormSubmit() {
+    const id = this.readForm.controls.tweetId.value;
+    if (id) {
+      await this.twitterService.searchTweet(id);
+    }
+  }
 }
